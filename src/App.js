42e0cbdb16b80b57751c9db1fc34246abe76403b
Victor Huggo <victorhuggo97@gmail.com>
2020-01-15 18:00:14 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-// Estado:
-// Propriedade:
-
-import Header from './Header';
+import './global.css';
+import './App.css';
 
 function App() {
   return (
-    <Header />
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+
+    </div>
   );
 }
 
